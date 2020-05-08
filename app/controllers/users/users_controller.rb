7df5e08b9e7ca55ed33c9a948759ba64ffe17ff5
堀田優介 <horiyuu@hottayuusukenoMacBook-Air.local>
2020-05-08 17:10:13 +0900

    Add model,controller,view,routes,gem

---
+++ app/controllers/users/users_controller.rb
@@ -0,0 +1,3 @@
+class Users::UsersController < ApplicationController
+
+end
---
+++ app/models/post.rb
@@ -0,0 +1,11 @@
+class Post < ApplicationRecord
+
+	validates :title, presence: true
+	validates :body, presence: true
+
+	has_many :comments, dependent: :destroy
+	has_many :likes, dependent: :destroy
+	belongs_to :user
+
+	attachment :image
+end
