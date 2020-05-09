34f26f2ff1c9bb4ed791d009c4e0d47f17fefe8b
堀田優介 <somagawa645@gmail.com>
2020-05-09 23:00:56 +0900

    Add Ajax(follow)

--- app/controllers/users/relationships_controller.rb
+++ app/controllers/users/relationships_controller.rb
@@ -1,13 +1,13 @@
 class Users::RelationshipsController < ApplicationController
 	def create
 		relationship = Relationship.new(following_id: current_user.id, follower_id: params[:user_id])
+		@user = relationship.follower
 		relationship.save
-		redirect_back(fallback_location: root_path)
 	end
 
 	def destroy
 		relationship = Relationship.find(params[:id])
+		@user = relationship.follower
 		relationship.destroy
-		redirect_back(fallback_location: root_path)
 	end
 end
