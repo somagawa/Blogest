5ad74b50b2819a577765e10f0c5c560430382cc5
堀田優介 <somagawa645@gmail.com>
2020-05-11 17:20:19 +0900

    Add image_preview_function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_10_141652) do
+ActiveRecord::Schema.define(version: 2020_05_11_061830) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 2020_05_10_141652) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "post_code"
+    t.string "address"
   end
 
   create_table "relationships", force: :cascade do |t|
